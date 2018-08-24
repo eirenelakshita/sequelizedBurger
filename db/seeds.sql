beea3ffa43a12721198996ef1ce99dda86b4e24b
unknown <laeirene59@gmail.com>
2018-08-24 13:35:19 -0400

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Beef Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Barbecue Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tuna Avocado Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Filet O Fish', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
